1d0f1d97be32ef958d34fbf203ef02dd29bc21dd
bz546219 <bzandruby2021@gmail.com>
2025-07-31 18:56:08 -0400

    s

--- Main.java
+++ Main.java
@@ -2,13 +2,11 @@ public class Main{
     static public void main(String[] args){
 
         //Variables
-        String studentName = "John";
-        int age = 16;
-        char grade = 'A';
-        double height = 5.9;
+        int length = 5, width = 6, area;
 
-        System.out.println(studentName + " is " + age + " years old.");
-        System.out.println(studentName + " is a straight " + grade + " student.");
-        System.out.println(studentName + " is " + 5.9 + "ft tall.");
+        //Calculate the area
+        area = length * width;
+
+        System.out.println(area);
     }
 }
