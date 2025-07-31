6deea7e0b7245733d62dd586e5fca61ae9188aff
bz546219 <bzandruby2021@gmail.com>
2025-07-31 16:19:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     static public void main(String[] args){
+        //This code will print Hello World and 6
         System.out.println("Hello World");
         System.out.println(3+3);
     }
